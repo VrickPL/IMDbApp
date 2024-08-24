ce1fcd009865f5e44339dd5dde4b05b18da35e09
Jan Kazubski <jkazubski@icloud.com>
2024-08-24 19:54:14 +0200

    Initial project commit

---
+++ IMDbApp/IMDbApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IMDbApp
+//
+//  Created by Jan Kazubski on 24/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IMDbApp/IMDbApp/IMDbAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMDbAppApp.swift
+//  IMDbApp
+//
+//  Created by Jan Kazubski on 24/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct IMDbAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
