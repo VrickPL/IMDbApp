511beeed68f1ac2e5402eef7341fadef3a669d62
Jan Kazubski <jkazubski@icloud.com>
2024-08-25 21:29:20 +0200

    Implemented tabview

---
+++ IMDbApp/IMDbApp/Views/WatchListView.swift
@@ -0,0 +1,18 @@
+//
+//  WatchListView.swift
+//  IMDbApp
+//
+//  Created by Jan Kazubski on 25/08/2024.
+//
+
+import SwiftUI
+
+struct WatchListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WatchListView()
+}
